fb6d0dff21b693d295d670e14c84e8d79f0db46e
Wise <Wise@DESKTOP-52B4CPM>
2022-03-25 14:24:28 -0300

    Conclusao Modulo 01

---
+++ cursoJavaAlura/src/Java/JRE/JDK/TestaVariaveis.java
@@ -0,0 +1,12 @@
+package Java.JRE.JDK;
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+	
+		System.out.println("ola novo teste");
+
+		int idade = 37;
+
+		System.out.println("a idade é " + idade + ", parabéns!");
+	}
+}
---
+++ cursoJavaAlura/src/Java/JRE/JDK/Testalacos.java
@@ -0,0 +1,36 @@
+package Java.JRE.JDK;
+
+public class Testalacos {
+
+	public static void main(String[] args) {
+		
+	
+//		for(int linha = 0; linha < 5; linha++) {
+//            for (int coluna = 0; coluna < 5; coluna++) {
+//                if ( coluna > linha ) {
+//                    break;
+//                }
+//                System.out.print(coluna+1);
+//            }
+//            System.out.println();
+//		}
+		
+		//imprimir todos os numeros divisiveis por 3
+		for(int linha = 0; linha <= 100 ; linha++) {
+			if(linha % 3 == 0) {
+				  // faça 
+				System.out.print(linha+ " ;");
+				}
+		}
+//		for (int linha = 1; linha <= 10; linha++) {
+//			for (int coluna = 0; coluna <= 10; coluna++) {
+//				// System.out.print(multiplicador * contador);
+//				if (coluna > linha) {
+//					break;
+//				}
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+	}
+}
